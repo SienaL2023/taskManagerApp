2638e99604655a747875b939074ebf90549e21b4
SienaL2023 <xiaosiena@gmail.com>
2025-11-23 18:04:40 -0800

    diag table

--- src/main/java/com/mytasks/ui/GUI.java
+++ src/main/java/com/mytasks/ui/GUI.java
@@ -1,12 +1,19 @@
 package com.mytasks.ui;
 
 import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 
+import com.mytasks.model.Priority;
 import com.mytasks.service.NotificationMgr;
 import com.mytasks.service.TaskManager;
 
@@ -25,33 +32,103 @@ public class GUI extends JFrame{
         setLocationRelativeTo(null);
         initUI();
 
-        // MAKE BUTTONS
-        // add button
-        JButton addBtn = new JButton("Add Task");
-        addBtn.addActionListener(e -> taskManager.addTask(task));
-        JButton viewBtn = new JButton("View Task");
-        viewBtn.addActionListener(e -> taskManager.listTasks());
-        JButton deleteBtn = new JButton("Delete Task");
-        deleteBtn.addActionListener(e -> taskManager.deleteTask());  // delete based on row not ID
-        // excel sheet
-        JPanel bottomPanel = new JPanel(new BorderLayout(5,5));
-        bottomPanel.add(addBtn, BorderLayout.WEST);
-        bottomPanel.add(viewBtn, BorderLayout.CENTER);
-        bottomPanel.add
+        
 
     }
 
-    JTable --> excel-like format
-    JList -- select rows/list
+    // JTable --> excel-like format
+    // JList -- select rows/list
 
     private void initUI(){
         // create the UI
         JPanel mainPanel = new JPanel(new BorderLayout());
+        JTable table;
 
+        String[][] data = {
+             {"0", "Test", "11:05", "high"},
+             {"1", "Test 1","11:07", "low"}
+         };
+        String [] columnNames = {"ID", "Title", "Deadline", "Prioity"};
+
+        table = new JTable(data, columnNames);
+        Font newFont = new Font("Comic Sans MS", Font.PLAIN, 14);
+        table.setFont(newFont);
+        JScrollPane scrollPane = new JScrollPane(table);
+        mainPanel.add(scrollPane);
+
+        // MAKE BUTTONS
+        // add button
+
+        // JFrame frame;
+        // JTable table;
+
+        // frame = new JFrame();
+        // frame.setTitle("JTable Example");
+
+        // // columns
+        // // ID  Deadline  Urgency
+        // // 0    11:05    high
+
+        // String[][] data = {
+        //     {"0", "11:05", "high"},
+        //     {"1", "11:07", "low"}
+        // };
+
+        // String [] columnNames = {"ID", "Deadline", "Urgency"};
+
+        // table = new JTable(data, columnNames);
+
+        // JScrollPane scrollPane = new JScrollPane(table);
+        // frame.add(scrollPane);
+        // frame.setSize(600,600);
+        // frame.setVisible(true);
+
+
+
+        JButton addBtn = new JButton("Add Task");
+        addBtn.setFont(newFont);
+        // addBtn.addActionListener(e -> taskManager.addTask(task));
+        JButton viewBtn = new JButton("View Task");
+        viewBtn.setFont(newFont);
+        // viewBtn.addActionListener(e -> taskManager.listTasks());
+
+        JButton deleteBtn = new JButton("Delete Task");
+        deleteBtn.setFont(newFont);
+        // deleteBtn.addActionListener(e -> taskManager.deleteTask());  // delete based on row not ID
+        table.getSelectedRow();
+        
+
+        JPanel btnPanel = new JPanel(new FlowLayout());
+        
+        mainPanel.add(btnPanel, BorderLayout.SOUTH);
+        btnPanel.add(addBtn, BorderLayout.WEST);
+        btnPanel.add(viewBtn, BorderLayout.CENTER);
+        btnPanel.add(deleteBtn, BorderLayout.EAST);
+
+        btnPanel.setFont(newFont);
         add(mainPanel);
+
+        
     }
 
+
     private void addTaskRow(){
-        JTextField field
+        // diag table:
+        // list out everything u need (task name, description, deadline, priority)
+        //      this will be a JTextField
+        // add all that info into a list
+
+        JTextField titleField = new JTextField();
+        JTextField descField = new JTextField();
+        JTextField dlField = new JTextField();
+        JComboBox<Priority> priBox = new JComboBox<>(Priority.values());
+         Object[] message = {
+            "Title: ", titleField,
+            "Desc: ", descField,
+            "Deadline(YYYY-MM-DDTHH:mm): ", dlField,
+            "Priority: ", priBox
+         };
+
+        JOptionPane.showConfirmDialog(titleField, message,"Add Task", JOptionPane.OK_CANCEL_OPTION);
     }
 }
